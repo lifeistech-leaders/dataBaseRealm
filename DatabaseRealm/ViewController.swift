41ddecf9b5bb9775ed9bc873166ace8fb176da9a
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-05-10 15:15:35 +0900

    first commit

---
+++ DatabaseRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatabaseRealm
+//
+//  Created by 大澤清乃 on 2024/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
